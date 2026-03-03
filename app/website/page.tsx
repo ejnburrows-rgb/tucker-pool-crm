b70c84bae46384ffc72fdef99f682774e5382e31
ejnburrows-rgb <ejnburrows@gmail.com>
2026-03-02 19:08:03 -0500

    Create page.tsx

---
+++ app/website/page.tsx
@@ -0,0 +1,8 @@
+export default function WebsitePage() {
+  return (
+    <main style={{ padding: "2rem", fontFamily: "sans-serif" }}>
+      <h1>Tucker Pool Services</h1>
+      <p>Welcome to the Tucker Pool Services website.</p>
+    </main>
+  );
+}
